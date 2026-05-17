323e3a943af88a48521601ed1a112e52df337d01
Nadeem Mazhar <nadeemmazhar12q@gmail.com>
2026-05-17 19:43:34 +0500

    Mount MCP security center

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
+import SecurityMount from "./SecurityMount.jsx";
 import "../styles.css";
 
 ReactDOM.createRoot(document.getElementById("app")).render(
   <React.StrictMode>
     <App />
+    <SecurityMount />
   </React.StrictMode>
 );
