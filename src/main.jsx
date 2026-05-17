02fb274b7c28fae799a49e04c1033a4e467e13bf
Nadeem Mazhar <nadeemmazhar12q@gmail.com>
2026-05-17 21:22:11 +0500

    Mount reminder center

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
+import ReminderCenter from "./ReminderCenter.jsx";
 import SecurityMount from "./SecurityMount.jsx";
 import "../styles.css";
 
 ReactDOM.createRoot(document.getElementById("app")).render(
   <React.StrictMode>
     <App />
+    <ReminderCenter />
     <SecurityMount />
   </React.StrictMode>
 );
